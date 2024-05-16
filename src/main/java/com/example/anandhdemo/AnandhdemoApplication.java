2ca123a15557ee6ddb8f04c6cec3840793e53be8
root <root@ip-172-31-49-60.ec2.internal>
2024-05-16 14:32:21 +0000

    first commit

---
+++ src/main/java/com/example/anandhdemo/AnandhdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.anandhdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnandhdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnandhdemoApplication.class, args);
+	}
+
+}
